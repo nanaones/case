f84106d9176dfc906852ed4d4f8a6099e6f693fe
nanaones <nara03050@gmail.com>
2020-02-16 06:00:45 +0900

    Change repo address
    change to start init.py when ends of wait-for-it script

--- Docker/Start.sh
+++ Docker/Start.sh
@@ -4,9 +4,10 @@ echo $DBMS_ADDRESS &&\
 echo $DBMS_PORT &&\
 echo $CONFI_GPATH &&\
 /docker/CheckStandAlone.sh &&\
-git clone https://github.com/nanare/wanted &&\
+git clone https://github.com/nanare/case &&\
 cd  /wanted/ &&\
 python3 -m pip install -r requirements.txt &&\
+python3 init.py &&\
 gunicorn --access-logfile  &&\
          /log/logaccess_log --error-logfile &&\
          /log/error_log -b 0.0.0.0:5000  &&\
